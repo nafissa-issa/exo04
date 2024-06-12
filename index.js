f669f1148b709648d321bd872f7ed8cbb1427169
Issa Oumarou Nafissa <nafissaissaoumarougmail.com@iMac-de-Issa.local>
2024-06-12 10:56:07 +0100

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ label2.innerHTML = `
 <input type="checkbox" name="refrain" id="masquer-paroles" /> Masquer les
       paroles`
 document.body.append(label2)
-
+label2.className = "paroles"
+console.log(label2);
 
 
 // 2.
@@ -33,11 +34,30 @@ chanson.paroles.forEach(element => {
 });
 
 // 3.
+
 let footer = document.createElement("footer")
 footer.textContent = " © Copyright 2023 - Codeloccol"
 document.body.append(footer)
-console.log(footer)
+
 
 // 4.
-let input1 = document.getElementById("masquer-refrains");
-console.log(input1);
+const checkboxParoles = document.getElementById("masquer-paroles"); 
+const divParoles = document.getElementById("paroles");
+let noeux = checkboxParoles.nextSibling
+let ligne = document.querySelector("hr")
+console.log(checkboxParoles);
+  
+// Attache une fonction à l'évènement "click" sur checkboxParoles
+checkboxParoles.addEventListener('click', function (event) {
+ 
+  if (event.target.checked) {
+   divParoles.style.display = "none"
+   noeux.textContent = "Afficher les paroles"
+   ligne.style.display = "none"
+
+  } else {
+    divParoles.style.display = "block"
+   noeux.textContent = "Masquer les paroles"
+
+  }
+});
